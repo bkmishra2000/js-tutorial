ce72b683a760ac3cc0240cc7c8fb202fafbfc528
bkmishra2000 <128887216+bkmishra2000@users.noreply.github.com>
2024-01-02 18:22:43 +0000

    container config done

---
+++ basic/variable.js
@@ -0,0 +1,14 @@
+const  accountId = 12344
+let accountEmail = "bkmishra2056@google.com"
+var accountPassword = "1234"
+
+accountCity = "mumbai"
+let accountState;
+//accountId = 4 .. not avalibal
+
+accountEmail = "brijesh@google"
+accountPassword = "121212"
+accountCity = "bhopal"
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
