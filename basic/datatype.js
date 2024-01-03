0cc8a6467ef8835177ea043506922e5c9598fb1c
bkmishra2000 <128887216+bkmishra2000@users.noreply.github.com>
2024-01-03 05:51:47 +0000

    datatype

---
+++ basic/datatype.js
@@ -0,0 +1,23 @@
+// data types 
+
+let name = "brijesh"
+let age = 23
+let state;
+let userLogIn = true
+
+// type of data type in javascript
+
+// 1. number => 2 to power 53
+// 2. String => in use "" 
+// 3. Boolean => True/false
+// 4. null => standalone value
+// 5. undefine => value not assign
+// 6. symbol => unique
+
+// 7. Object
+
+
+console.log(typeof age)
+console.log(typeof "brijesh")
+console.log(typeof undefine) // undefine
+console.log(typeof null) // object
