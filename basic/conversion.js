883643b1ebf2b1e9d1081d765189ab00f385138f
bkmishra2000 <128887216+bkmishra2000@users.noreply.github.com>
2024-01-05 06:17:14 +0000

    oparetion

---
+++ basic/conversion.js
@@ -0,0 +1,51 @@
+let someNumber = 33
+
+let stringNumber = String(someNumber) // conversion number to string using variable
+
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//  ************** Oparetion **************
+
+let value = 3
+let nagvalue = -value
+
+// console.log(nagvalue)
+
+
+// mathamatic operator---------
+// console.log(2 + 2); // addition
+// console.log(2 - 2); // subtract
+// console.log(2 * 2);// multipication
+// console.log(2 ** 3);// power of
+// console.log(2 / 2);// divion
+// console.log(2 % 2);// modulas
+
+
+let str1 = "hello"
+let str2 = " brijesh"
+let str3 = str1 + str2
+
+//console.log(str3)
+
+// console.log( 1 + "2"); // convert in string
+// console.log( "1" + 2); // convert into string
+// console.log( 1 + "2" + 2); 
+// console.log( 1 + 2 + "2");// fist come number than convert number
+// console.log("1" + 2 + 2);// fist come string than convert string
+
+
+// console.log(true)
+// console.log(+true)
+
+// console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+// console.log(num1)
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
